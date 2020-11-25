05b372ff1ce3a6f57d48ecc70123019d103c2e9f
zaini <alimzaini@outlook.com>
2020-11-25 00:43:15 +0000

    Change clashing routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,12 @@ Rails.application.routes.draw do
   get 'user/new'
   get 'user/edit'
   get 'user/delete'
-  
+
   # get 'manage_users/create_user'
-  match 'admin/create', :to => 'manage_users#create_user', :via => :get
+  match 'admin/manage_users/create', :to => 'manage_users#create_user', :via => :get
   # get 'manage_users/edit_user'
-  match 'admin/edit', :to => 'manage_users#edit_user', :via => :get
+  match 'admin/manage_users/edit', :to => 'manage_users#edit_user', :via => :get
   # get 'manage_users/delete_user'
-  match 'admin/delete', :to => 'manage_users#delete_user', :via => :get
+  match 'admin/manage_users/delete', :to => 'manage_users#delete_user', :via => :get
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
