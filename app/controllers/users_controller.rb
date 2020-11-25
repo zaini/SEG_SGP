eb7f2df7ddbd19addd9ef883b18152372ce54984
zaini <alimzaini@outlook.com>
2020-11-25 03:36:25 +0000

    Add CRUD operations for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,51 @@
 class UsersController < ApplicationController
   def index
+    @users = User.order(:id)
   end
 
   def show
-    @user = Users.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to(users_path)
+    else
+      render('new')
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to(user_path(@user))
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @user = User.find(params[:id])
   end
 
-  def destory
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to(users_path)
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :middle_name, :last_name, :email, :password)
+    end
+
 end
