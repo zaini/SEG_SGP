2179c46bb67bbb1a0d4bdface61816f273bab5cd
grumpykendal0622 <k19035556@kcl.ac.uk>
2020-12-15 01:47:39 +0800

    Add correct testing for transaction controller.

--- test/controllers/transactions_controller_test.rb
+++ test/controllers/transactions_controller_test.rb
@@ -2,38 +2,44 @@ require 'test_helper'
 
 class TransactionsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @admin = Admin.create(username: "admin", email: "admin@email.com", email_confirmation: "admin@email.com", password: "admin123", password_confirmation: "admin123")
-    @user = User.new(admin: @admin, first_name: "user", middle_name: "m", last_name: "last", email: "user@email.com", email_confirmation: "user@email.com", password: "user123", password_confirmation: "user123")
+    @admin = Admin.create!(username: "admi", email: "admi@email.com", email_confirmation: "admi@email.com", password: "admin123", password_confirmation: "admin123")
+    @user = User.create!(admin: @admin, first_name: "ha", middle_name: "m", last_name: "last", email: "ha@email.com", email_confirmation: "ha@email.com", password: "user123", password_confirmation: "user123")
     @account = BankAccount.create!(user:@user, account_name:"account_name", account_number:'5355220468996651', sort_code:'000000')
+    @transaction = Transaction.create!(bank_account:@account, date:'2020-12-01 15:48:49', description:'@transaction1.description', reference:'@transaction1.reference', money_in:100, money_out:20)
+    
   end
 
   test "should get index" do
-    get :"/bank_account_transactions_path"
+    get bank_account_transactions_path(@account)
     assert_response :success  
   end
 
   test "should get new" do
-    get :"/new_bank_account_transaction_path"
+    get new_bank_account_transaction_path(@account)
     assert_response :success
   end
 
   test "post create should work with valid data" do
     assert_difference('Transaction.count',1) do
-      post :"/create_bank_account_transaction_path(@account)", params: {transaction: {bank_account_id:@account.id, date:'2020-12-01 15:48:49', description:"description", reference:"reference", money_in:'0', money_out:'100'}}
+      post bank_account_transactions_path(@account), params: {bank_account:@account,transaction: {date:'2020-12-01 15:48:49', description:'@transaction1.description', reference:'@transaction1.reference', money_in:100, money_out:10}}
     end
-    assert_redirect_to bank_account_transactions_path(@account)
+    assert_redirected_to bank_account_transactions_path(@account)
   end
 
   test "should get update" do
-    put :"/update", params: {transaction: {date:'2020-12-01 15:48:49', description:"description", reference:"reference", money_in:'0', money_out:'100'}}
-    assert_response :success
+    # t = transactions(:one)
+    patch bank_account_transaction_path(@account,@transaction), params:  {bank_account:@account,transaction: {date:'2020-12-05 15:48:49', description:'@transaction1.description', reference:'@transaction1.reference', money_in:1000, money_out:0}}
+    assert_redirected_to bank_account_transactions_path(@account)
+    @transaction.reload
+    assert_equal "2020-12-05 15:48:49", @transaction.date
   end
 
   test "should get destroy" do
     transaction = transactions(:one)
+    # transaction.bank_account = @account
     assert_difference('Transaction.count',-1) do
-      delete :"/destroy_bank_account_transaction_path(@account,transaction)"
+      delete bank_account_transaction_path(@account,@transaction)
     end
-    assert_redirect_to bank_account_transactions_path(@account)
+    assert_redirected_to bank_account_transactions_path(@account)
   end
 end
