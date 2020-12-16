df377dd893f86fd7e92153ccca8857d2eb865534
teyjianhan <jasonteyjianhan@gmail.com>
2020-12-17 07:50:13 +0800

    Finish main tests of admin controller.

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,29 +1,40 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get admin_index_url
-  #   assert_response :success
-  # end
+  setup do
+    @admin = Admin.create!(username:'ABCDE', email:'ABCDE@email.com', email_confirmation:'ABCDE@email.com', password:'12345', password_confirmation:'12345')
+  end
 
-  # test "should get show" do
-  #   get admin_show_url
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get admin_path
+    assert_response :success
+  end
 
-  # test "should get new" do
-  #   get admin_new_url
-  #   assert_response :success
-  # end
+  test "should get new" do
+     get new_admin_path
+     assert_response :success
+  end
 
-  # test "should get edit" do
-  #   get admin_edit_url
-  #   assert_response :success
-  # end
+  test "post create should work with valid data" do
+    assert_difference('Admin.count',1) do
+      post admin_path, params: {username:'ABCDE', email:'ABCDE@email.com', email_confirmation:'ABCDE@email.com', password:'12345', password_confirmation:'12345'}
+      #:username, :email, :email_confirmation, :password, :password_confirmation
+    end
+    assert_redirected_to admin_path
+  end
 
-  # test "should get delete" do
-  #   get admin_delete_url
-  #   assert_response :success
-  # end
+  test "should get update" do
+    patch admin_path(@admin), params: {username:'ABCDEF', email:'ABCDE@email.com', email_confirmation:'ABCDE@email.com', password:'12345', password_confirmation:'12345'}
+    assert_redirected_to admin_path
+    @admin.reload
+    assert_equal "ABCDEF", @admin.username
+  end
+
+  test "should get destroy" do
+    assert_difference('Admin.count',-1) do
+      delete admin_path(@admin)
+    end
+    assert_redirected_to admin_path
+  end
 
 end
