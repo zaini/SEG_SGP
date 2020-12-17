7e17ee396a5cc953f0de66ec5008a4d2ef1c3ae2
teyjianhan <jasonteyjianhan@gmail.com>
2020-12-17 08:41:36 +0800

    Finalise currency model & test.

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,5 @@
 class Currency < ApplicationRecord
+  validates :code, presence: true, uniqueness: true
+  validates :symbol, presence: true
+  validates :rate_to_gbp, presence: true
 end
--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @admin = Admin.create!(username:'ABCDE', email:'ABCDE@email.com', email_confirmation:'ABCDE@email.com', password:'12345', password_confirmation:'12345')
+    @admin = Admin.create!(username:'ABCDE', email:'ABCDE@email.com', email_confirmation:'ABCDE@email.com', password:'123456', password_confirmation:'123456')
   end
 
   test "should get index" do
-    get admin_path
+    get admins_path
     assert_response :success
   end
 
@@ -17,15 +17,15 @@ class AdminControllerTest < ActionDispatch::IntegrationTest
 
   test "post create should work with valid data" do
     assert_difference('Admin.count',1) do
-      post admin_path, params: {username:'ABCDE', email:'ABCDE@email.com', email_confirmation:'ABCDE@email.com', password:'12345', password_confirmation:'12345'}
+      post admin_path, params: {username:'ABCDE', email:'ABCDE@email.com', email_confirmation:'ABCDE@email.com', password:'123456', password_confirmation:'123456'}
       #:username, :email, :email_confirmation, :password, :password_confirmation
     end
-    assert_redirected_to admin_path
+    assert_redirected_to admin_login_path
   end
 
   test "should get update" do
-    patch admin_path(@admin), params: {username:'ABCDEF', email:'ABCDE@email.com', email_confirmation:'ABCDE@email.com', password:'12345', password_confirmation:'12345'}
-    assert_redirected_to admin_path
+    patch admin_path(@admin), params: {username:'ABCDEF', email:'ABCDE@email.com', email_confirmation:'ABCDE@email.com', password:'123456', password_confirmation:'123456'}
+    assert_redirected_to admin_path(@admin)
     @admin.reload
     assert_equal "ABCDEF", @admin.username
   end
@@ -34,7 +34,7 @@ class AdminControllerTest < ActionDispatch::IntegrationTest
     assert_difference('Admin.count',-1) do
       delete admin_path(@admin)
     end
-    assert_redirected_to admin_path
+    assert_redirected_to admin_path(@admin)
   end
 
 end
