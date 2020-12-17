45aef7089789590c7dd88f89ba1cf78a7839c6c2
teyjianhan <jasonteyjianhan@gmail.com>
2020-12-17 21:36:34 +0800

    Update admin & user test (needs modification)

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -5,6 +5,7 @@ class UserControllerTest < ActionDispatch::IntegrationTest
      @admin = Admin.create!(username: "banana", email: "banana@email.com", email_confirmation: "banana@email.com", password: "banana", password_confirmation: "banana")
      @currency = Currency.create!(code: "KCL", symbol: "$", rate_to_gbp:1.1)
      @user = User.create!(admin: @admin, first_name: "Grape", middle_name: "Grape", last_name: "Grape", email: "grape@email.com", email_confirmation: "grape@email.com", password: "grape1", password_confirmation: "grape1", currency_id: @currency)
+     #SessionsController.create_admin!()
   end
 
   test "should get index" do
@@ -14,33 +15,32 @@ class UserControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
-     #get "/signup_user"
+     #get signup_user_url
      get "/users/new"
      assert_response :success
   end
 
   test "post create should work with valid data" do
     assert_difference('User.count',1) do
-      post users_path, params: {admin: @admin, first_name:"Foo", middle_name:"Bar", last_name:"Foo", email:"foobar@email.com", email_confirmation:"foobar@email.com", password:"foobar", password_confirmation:"foobar", currency_id:@currency}
+      post users_path, params: {user:{admin: @admin, first_name:"Foo", middle_name:"Bar", last_name:"Foo", email:"foobar@email.com", email_confirmation:"foobar@email.com", password:"foobar", password_confirmation:"foobar", currency_id:@currency}}
       #:first_name, :middle_name, :last_name, :email, :email_confirmation, :password, :password_confirmation, :currency_id
     end
     assert_redirected_to users_path
   end
 
   test "should get update" do
-    patch users_path(@user), params:  {admin: @admin, first_name: "Apple", middle_name: "Grape", last_name: "Grape", email: "grape@email.com", email_confirmation: "grape@email.com", password: "grape1", password_confirmation: "grape1", currency_id: @currency}
+    patch users_path(@user), params:  {user:{admin: @admin, first_name: "Apple", middle_name: "Grape", last_name: "Grape", email: "grape@email.com", email_confirmation: "grape@email.com", password: "grape1", password_confirmation: "grape1", currency_id: @currency}}
     assert_redirected_to user_path(@user)
     @user.reload
     assert_equal "Apple", @user.first_name
   end
 
   test "should get destroy" do
+    user = users(:one)
     assert_difference('User.count',-1) do
       delete user_path(@user)
     end
     assert_redirected_to users_path
   end
-
-
 
 end
