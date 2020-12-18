aaa980b1337ed5fc53c60ad5815f81ce62e2b90c
PaX404 <quan.zhou@kcl.ac.uk>
2020-12-18 09:17:11 +0000

    Add bank accounts controller test. Remove homepage controller test.

--- test/controllers/homepage_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class HomepageControllerTest < ActionDispatch::IntegrationTest
-  test "should get homepage" do
-    get homepage_homepage_url
-    assert_response :success
-  end
-
-end
